3e83c8315abbd9f2db1a7eba42d609257139aba0
rolarbear <jasonromine@cox.net>
2013-11-04 00:12:12 -0800

    start with NRF24 communication

--- ScramblesTheDeathDealer.c
+++ ScramblesTheDeathDealer.c
@@ -2,33 +2,33 @@
 * ScramblesTheDeathDealer.c
 *
 *	PORTB
-*	0	
+*	0
 *	1	CE pin on NRF24L01+
 *	2	SS(CS) pin on NRF24L01+
 *	3	MOSI pin on NRF24L01+
 *	4	MISO pin on NRF24L01+
 *	5	SCK pin on NRF24L01+
-*	6	
-*	7	
-*	
+*	6
+*	7
+*
 *	PORTC
 *	0	Motor analog stick X
 *	1	Motor analog stick Y
 *	2	(Servo control stick X)
 *	3	(Servo control stick Y)
-*	4	
-*	5	
+*	4
+*	5
 *	6	RESET
-*	
+*
 *	PORTD
 *	0	DEBUG RX
 *	1	DEBUT TX
 *	2	Left motor direction
 *	3	Right motor direction
-*	4	
+*	4
 *	5	Left motor PWM
 *	6	Right motor PWM
-*	7	
+*	7
 *
 * Created: 10/10/2013 12:46:50 AM
 *  Author: poop
@@ -42,7 +42,7 @@
 *	NRF24L01 COMMUNICATION
 *	MAKE CIRCUIT BOARD
 *	???
-*	
+*
 *  CONTROL:
 *	NRF24L01 COMMUNICATION
 *	HACK OPEN CHEAP CONTROLLER
@@ -52,10 +52,12 @@
 */
 
 #define F_CPU 16000000
+#define USART_BAUDRATE 9600
+#define BAUD_PRESCALE (((16000000 / (USART_BAUDRATE * 16UL)))-1)
 
 #include <avr/io.h>
 #include <util/delay.h>
-#include </nRF24L01.h>
+#include "nRF24L01.h"
 
 void motorControl(int m1Duty, char m1Dir , int m2Duty, char m2Dir);
 long map(long x, long in_min, long in_max, int out_min, int out_max);
@@ -69,53 +71,136 @@ void driveForward(int dutym1, int dutym2);
 void driveBackward(int dutym1, int dutym2);
 void turnLeft(int dutym1, int dutym2);
 void turnRight(int dutym1, int dutym2);
+void setupSPI(void);
 void enableSPI(void);
-void transmitSPI(char cData);
+void disableSPI(void);
+char write_read_SPI(char cData);
+uint8_t GetReg(uint8_t reg);
+void writeReg(uint8_t reg, uint8_t val);
 
 volatile long adc0Rd, adc1Rd;
 int m1,m2;
 char m1d, m2d;
+int diagPrint = 1;
 
 int main(void)
 {
+	//Setup SPI pins PB1 CE, PB2 SS(CS) OUTPUT, PB3 MOSI OUTPUT, PB4 MISO INPUT, PB5 SCK OUTPUT
+	DDRB |= (1<<DDB1)|(1<<DDB2)|(1 << DDB3)|(1 << DDB5);
+	//	DDRB &= ~(1 << DDB4);
 	//Enable PD2 and PD3 for direction and PD5 and PD6 for PWM
 	DDRD |= (1 << DDD2)|(1 << DDD3)|(1 << DDD5)|(1 << DDD6);
 	//Enable PC0 for motor analog stick X and PC1 for motor analog stick Y
 	DDRC |= (1 << DDC0)|(1 << DDC1);
 	
-	enableADC();
-	_delay_ms(10);
+	UBRR0L = BAUD_PRESCALE;
+	UBRR0H = (BAUD_PRESCALE >> 8);
+	//enable transmit and receive lines
+	UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
+	//8 data bits, 1 stop bits
+	UCSR0C = (3 << UCSZ00);
 	
-	while(1)
+	while ((UCSR0A & (1 << UDRE0)) == 0) {};
+	UDR0 = ' ';
+	while ((UCSR0A & (1 << UDRE0)) == 0) {};
+
+	setupSPI();
+
+	//enableADC();
+	_delay_ms(10);
+
+	while (1)
 	{
-		
-		readJoysticks(&m1,&m2,&m1d,&m2d);
-		
-		motorControl(m1, m1d, m2, m2d);
+		uint8_t stat = GetReg(STATUS);
+		if(diagPrint == 1)
+		{
+			while ((UCSR0A & (1 << UDRE0)) == 0) {};
+			UDR0 = stat;
+			while ((UCSR0A & (1 << UDRE0)) == 0) {};
+		}
+		while(stat != 0x0E){};
+		writeReg(RF_CH,0x07);
+		while ((UCSR0A & (1 << UDRE0)) == 0) {};
+		UDR0 = (GetReg(RF_CH));
+		while ((UCSR0A & (1 << UDRE0)) == 0) {};
+		writeReg(RF_SETUP, 0x06);
+		while ((UCSR0A & (1 << UDRE0)) == 0) {};
+		UDR0 = (GetReg(RF_SETUP));
+		while ((UCSR0A & (1 << UDRE0)) == 0) {};
+		_delay_ms(50);
+	}
+	//while(1)
+	//{
+	//	readJoysticks(&m1,&m2,&m1d,&m2d);
+	//	motorControl(m1, m1d, m2, m2d);
+	//}
+}
+
+uint8_t GetReg(uint8_t reg)
+{
+	//CSN low
+	PORTB &= ~(1 << DDB2);
+	//Set R_Register
+	write_read_SPI(R_REGISTER + reg);
+	//Send dummy receive register value
+	reg = write_read_SPI(NOP);
+	//CSN high
+	PORTB |= (1 << DDB2);
+	//return registry value that was read
+	return reg;
+}
+
+void writeReg(uint8_t reg, uint8_t val)
+{
+	//CSN low
+	PORTB &= ~(1 << DDB2);
+	
+	//Write register value
+	SPDR = reg;
+	//Wait for transmission complete
+	while(!(SPSR & (1<<SPIF)));
+	
+	//Write Value to go into register
+	SPDR = val;
+	//Wait for transmission complete
+	while(!(SPSR & (1<<SPIF)));
+	
+	//CSN high
+	PORTB |= (1 << DDB2);
+}
+
+void setupSPI(void)
+{
+	//Enable SPI, Master, set clock rate
+	SPCR |= (1<<SPE)|(1<<MSTR);
+	
+	//SETBIT(PORTB, 2);
+	PORTB |= (1<<DDB2);
+	//CLEARBIT(PORTB, 1);
+	if (PORTB & (1 << DDB1))
+	{
+		PORTB &= ~(1<<DDB1);
 	}
 }
 
 void enableSPI(void)
 {
-	//Enable PB1 for CE pin on NRF24L01+
-	DDRB |= (1<<DDB1);
-	//Setup SPI pins PB2 SS(CS) OUTPUT, PB3 MOSI OUTPUT, PB4 MISO INPUT, PB5 SCK OUTPUT
-	DDRB |= (1<<DDB2)|(1 << DDB3)|(1 << DDB5);
-	DDRB &= ~(1 << DDB4);
-	
-	//Set MOSI and SCK output, all others input
-	DDR_SPI = (1<<DD_MOSI)|(1<<DD_SCK)|~(1<<DD_MISO);
-	
-	//Enable SPI, Master, set clock rate fck/16
-	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+	SPCR |= (1<<SPE);
 }
-void transmitSPI(char cData)
+
+void disableSPI(void)
+{
+	SPCR &= ~(1<<SPE);
+}
+
+char write_read_SPI(char cData)
 {
 	//Start transmission
 	SPDR = cData;
 	//Wait for transmission complete
-	while(!(SPSR & (1<<SPIF)))
-	;
+	while(!(SPSR & (1<<SPIF)));
+	
+	return SPDR;
 }
 
 long map(long x, long in_min, long in_max, int out_min, int out_max)
@@ -390,14 +475,14 @@ void readJoysticks (int* xVal, int* yVal, char* leftDir, char* rightDir)
 	ch0Rd = readADC(0);
 	stopADC();
 	_delay_ms(10);
-//	ch0Rd = readADC(0);
-//	_delay_ms(10);
+	//	ch0Rd = readADC(0);
+	//	_delay_ms(10);
 	startADC();
 	ch1Rd = readADC(1);
 	stopADC();
 	_delay_ms(10);
-//	ch1Rd = readADC(1);
-//	_delay_ms(10);
+	//	ch1Rd = readADC(1);
+	//	_delay_ms(10);
 	
 	//map the adc reads and determine direction
 	if (ch0Rd < 512)
