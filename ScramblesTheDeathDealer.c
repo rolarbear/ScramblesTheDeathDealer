79ef6e9a6c25fd5ca74faeba9fcf4072a45c0f94
rolarbear <jason.romine84@gmail.com>
2013-12-10 22:58:05 -0800

    ADC fix
    inputs shouldn't be outputs

--- ScramblesTheDeathDealer.c
+++ ScramblesTheDeathDealer.c
@@ -76,16 +76,14 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdlib.h>
 #include "nRF24L01.h"
 
 void motorControl(int m1Duty, uint8_t m1Dir , int m2Duty, uint8_t m2Dir);
-long map(long x, long in_min, long in_max, int out_min, int out_max);
+int map(long x, long in_min, long in_max, int out_min, int out_max);
 void enableADC(void);
-uint16_t readADC(uint8_t channel);
-void readJoysticks (int* xVal, int* yVal, uint8_t* leftDir, uint8_t* rightDir);
-long mapRev(long x, long in_min, long in_max, int out_min, int out_max);
-void startADC(void);
-void stopADC(void);
+int readADC(uint8_t channel);
+void readMotorJoysticks (int* xVal, int* yVal, uint8_t* leftDir, uint8_t* rightDir);
 void driveForward(int dutym1, int dutym2);
 void driveBackward(int dutym1, int dutym2);
 void turnLeft(int dutym1, int dutym2);
@@ -98,21 +96,19 @@ uint8_t GetReg(uint8_t reg);
 void writeReg(uint8_t reg, uint8_t val);
 void setCh(uint8_t chan);
 
-
-volatile long adc0Rd, adc1Rd;
+volatile int adc0Rd, adc1Rd;
 int m1,m2;
 uint8_t m1d, m2d;
 int diagPrint = 1;
+uint8_t m1d_O, m2d_O;
 
 int main(void)
 {
 	//Setup SPI pins PB1 CE, PB2 SS(CS) OUTPUT, PB3 MOSI OUTPUT, PB4 MISO INPUT, PB5 SCK OUTPUT
 	DDRB |= (1<<DDB1)|(1<<DDB2)|(1 << DDB3)|(1 << DDB5);
-	//	DDRB &= ~(1 << DDB4);
+
 	//Enable PD2 and PD3 for direction and PD5 and PD6 for PWM
 	DDRD |= (1 << DDD2)|(1 << DDD3)|(1 << DDD5)|(1 << DDD6);
-	//Enable PC0 for motor analog stick X and PC1 for motor analog stick Y
-	DDRC |= (1 << DDC0)|(1 << DDC1);
 	
 	UBRR0L = BAUD_PRESCALE;
 	UBRR0H = (BAUD_PRESCALE >> 8);
@@ -125,11 +121,12 @@ int main(void)
 	UDR0 = ' ';
 	while ((UCSR0A & (1 << UDRE0)) == 0) {};
 
-	setupSPI();
+	//setupSPI();
 
-	//enableADC();
+	enableADC();
 	_delay_ms(10);
 
+/*
 	while (1)
 	{
 		uint8_t stat = GetReg(STATUS);
@@ -146,11 +143,48 @@ int main(void)
 		writeReg(RF_SETUP, 0x2E);
 		_delay_ms(50);
 	}
-	//while(1)
-	//{
-	//	readJoysticks(&m1,&m2,&m1d,&m2d);
-	//	motorControl(m1, m1d, m2, m2d);
-	//}
+*/
+	while(1)
+	{
+		readMotorJoysticks(&m1,&m2,&m1d,&m2d);
+		motorControl(m1, m1d, m2, m2d);
+		
+		/*
+		int i = 0;
+		
+		for(i=0;i<256;i=i+2)
+		{
+			motorControl(i,1,i,1);
+			_delay_ms(50);
+		}
+		
+		_delay_ms(500);
+		
+		for(i=0;i<255;i=i+2)
+		{
+			motorControl(i,1,i,1);
+			_delay_ms(50);
+		}
+		
+		_delay_ms(500);
+		
+		for(i=0;i<256;i=i+2)
+		{
+			motorControl(i,0,i,0);
+			_delay_ms(50);
+		}
+		
+		_delay_ms(500);
+		
+		for(i=0;i<255;i=i+2)
+		{
+			motorControl(i,0,i,0);
+			_delay_ms(50);
+		}
+		
+		_delay_ms(500);
+		*/
+	}
 }
 
 uint8_t flushRx(void)
@@ -241,223 +275,30 @@ char write_read_SPI(char cData)
 	return SPDR;
 }
 
-long map(long x, long in_min, long in_max, int out_min, int out_max)
+int map(long x, long in_min, long in_max, int out_min, int out_max)
 {
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-long mapRev(long x, long in_min, long in_max, int out_min, int out_max)
-{
-	return (x - in_min) * (out_min - out_max) / (in_max - in_min) + out_min;
-}
-
 void enableADC(void)
 {
-	//128 prescaller and enable the ADC
-	ADCSRA |= (1 << ADPS2)|(1 << ADPS1)|(1 << ADPS0)|(1 << ADEN);
 	//AVCC set to AREF voltage
-	ADMUX |= (1 << REFS0)/*|(1 << REFS1)*/;
-	
-	ADMUX |= (1 << ADLAR);
+	ADMUX |= (1 << REFS0);
+	//128 prescaller and enable the ADC
+	ADCSRA |= (1 << ADEN)|(1 << ADPS2)|(1 << ADPS1)|(1 << ADPS0);
 	
 	//Disable digital inputs on ADC lines
 	DIDR0 |= (1 << ADC0D)|(1 << ADC1D)|(1 << ADC2D)|(1 << ADC3D)|(1 << ADC4D)|(1 << ADC5D);
 }
 
-void startADC(void)
-{
-	ADCSRA |= (1 << ADEN);
-}
-
-void stopADC(void)
-{
-	ADCSRA &= ~(1 << ADEN);
-}
-
 //channel 0 - 8
-uint16_t readADC(uint8_t channel)
-{
-	//case statement for selecting the channel
-	//This sets up ADMUX registers
-	switch (channel)
-	{
-		case 0:
-		if(ADMUX & (1 << MUX0))
-		{
-			ADMUX &= ~(1 << MUX0);
-		}
-		if(ADMUX & (1 << MUX1))
-		{
-			ADMUX &= ~(1 << MUX1);
-		}
-		if(ADMUX & (1 << MUX2))
-		{
-			ADMUX &= ~(1 << MUX2);
-		}
-		if(ADMUX & (1 << MUX3))
-		{
-			ADMUX &= ~(1 << MUX3);
-		}
-		break;
-		
-		case 1:
-		if(ADMUX & ~(1 << MUX0))
-		{
-			ADMUX |= (1 << MUX0);
-		}
-		if(ADMUX & (1 << MUX1))
-		{
-			ADMUX &= ~(1 << MUX1);
-		}
-		if(ADMUX & (1 << MUX2))
-		{
-			ADMUX &= ~(1 << MUX2);
-		}
-		if(ADMUX & (1 << MUX3))
-		{
-			ADMUX &= ~(1 << MUX3);
-		}
-		break;
-		
-		case 2:
-		if(ADMUX & (1 << MUX0))
-		{
-			ADMUX &= ~(1 << MUX0);
-		}
-		if(ADMUX & ~(1 << MUX1))
-		{
-			ADMUX |= (1 << MUX1);
-		}
-		if(ADMUX & (1 << MUX2))
-		{
-			ADMUX &= ~(1 << MUX2);
-		}
-		if(ADMUX & (1 << MUX3))
-		{
-			ADMUX &= ~(1 << MUX3);
-		}
-		break;
-		
-		case 3:
-		if(ADMUX & ~(1 << MUX0))
-		{
-			ADMUX |= (1 << MUX0);
-		}
-		if(ADMUX & ~(1 << MUX1))
-		{
-			ADMUX |= (1 << MUX1);
-		}
-		if(ADMUX & (1 << MUX2))
-		{
-			ADMUX &= ~(1 << MUX2);
-		}
-		if(ADMUX & (1 << MUX3))
-		{
-			ADMUX &= ~(1 << MUX3);
-		}
-		break;
-		
-		case 4:
-		if(ADMUX & (1 << MUX0))
-		{
-			ADMUX &= ~(1 << MUX0);
-		}
-		if(ADMUX & (1 << MUX1))
-		{
-			ADMUX &= ~(1 << MUX1);
-		}
-		if(ADMUX & ~(1 << MUX2))
-		{
-			ADMUX |= (1 << MUX2);
-		}
-		if(ADMUX & (1 << MUX3))
-		{
-			ADMUX &= ~(1 << MUX3);
-		}
-		break;
-		
-		case 5:
-		if(ADMUX & ~(1 << MUX0))
-		{
-			ADMUX |= (1 << MUX0);
-		}
-		if(ADMUX & (1 << MUX1))
-		{
-			ADMUX &= ~(1 << MUX1);
-		}
-		if(ADMUX & ~(1 << MUX2))
-		{
-			ADMUX |= (1 << MUX2);
-		}
-		if(ADMUX & (1 << MUX3))
-		{
-			ADMUX &= ~(1 << MUX3);
-		}
-		break;
-		
-		case 6:
-		if(ADMUX & (1 << MUX0))
-		{
-			ADMUX &= ~(1 << MUX0);
-		}
-		if(ADMUX & ~(1 << MUX1))
-		{
-			ADMUX |= (1 << MUX1);
-		}
-		if(ADMUX & ~(1 << MUX2))
-		{
-			ADMUX |= (1 << MUX2);
-		}
-		if(ADMUX & (1 << MUX3))
-		{
-			ADMUX &= ~(1 << MUX3);
-		}
-		break;
-		
-		case 7:
-		if(ADMUX & ~(1 << MUX0))
-		{
-			ADMUX |= (1 << MUX0);
-		}
-		if(ADMUX & ~(1 << MUX1))
-		{
-			ADMUX |= (1 << MUX1);
-		}
-		if(ADMUX & ~(1 << MUX2))
-		{
-			ADMUX |= (1 << MUX2);
-		}
-		if(ADMUX & (1 << MUX3))
-		{
-			ADMUX &= ~(1 << MUX3);
-		}
-		break;
-		
-		case 8:
-		if(ADMUX & (1 << MUX0))
-		{
-			ADMUX &= ~(1 << MUX0);
-		}
-		if(ADMUX & (1 << MUX1))
-		{
-			ADMUX &= ~(1 << MUX1);
-		}
-		if(ADMUX & (1 << MUX2))
-		{
-			ADMUX &= ~(1 << MUX2);
-		}
-		if(ADMUX & ~(1 << MUX3))
-		{
-			ADMUX |= (1 << MUX3);
-		}
-		break;
-		
-		default:
-		break;
-	}
+int readADC(uint8_t channel)
+{	
+	uint8_t low, high;
 	
-	//enable ADC
-	ADCSRA |= (1 << ADEN);
+	//set the ADC channel
+	channel &= 0b00000111;
+	ADMUX = (ADMUX & 0xF8)|channel;
 	
 	//Start conversion
 	ADCSRA |= (1 << ADSC);
@@ -465,37 +306,26 @@ uint16_t readADC(uint8_t channel)
 	//WAIT FOR IT!!!
 	while(ADCSRA & (1 << ADSC));
 	
-	//disable ADC
-	ADCSRA &= ~(1 << ADEN);
+	low  = ADCL;
+	high = ADCH;
 	
 	//BOOM! RETURN THAT SHIT
-	return ADC;
+	return ((high << 8) | low);
+	//return ADC;
 }
 
 //m1Duty and m2Duty should be between 0 and 255
 void motorControl(int m1Duty, uint8_t m1Dir , int m2Duty, uint8_t m2Dir)
 {
-	//Set PWM value
-	OCR0A = (m1Duty);
-	OCR0B = (m2Duty);
-	
-	//setup pwm
-	TCCR0A |= (1 << COM0A1)|(1 << COM0B1);
-	TCCR0A |= (1 << WGM01)|(1 << WGM00);
-	
-	//Start that SHIT!
-	TCCR0B |= (1 << CS00)|(1 << CS02);
-	
+
 	//Set direction for motor 1
 	switch (m1Dir)
 	{
 		case 0:
-		CLEARBIT(PORTD, DDD2);
-		//PORTD &= ~_BV(PD2);
+			PORTD &= ~(1 << DDD2);
 		break;
 		case 1:
-		SETBIT(PORTD, DDD2);
-		//PORTD |= _BV(PD2);
+			PORTD |= (1 << DDD2);
 		default:
 		break;
 	}
@@ -504,58 +334,80 @@ void motorControl(int m1Duty, uint8_t m1Dir , int m2Duty, uint8_t m2Dir)
 	switch (m2Dir)
 	{
 		case 0:
-		CLEARBIT(PORTD, DDD3);
-		//PORTD &= ~_BV(PD3);
+			PORTD &= ~(1 << DDD3);
 		break;
 		case 1:
-		SETBIT(PORTD, DDD3);
-		//PORTD |= _BV(PD3);
+			PORTD |= (1 << DDD3);
 		break;
 		default:
 		break;
 	}
+	
+		//Set PWM value
+		MOTOR1PWM = (m1Duty);
+		OCR0B = (m2Duty);
+		
+		//setup pwm
+		TCCR0A |= (1 << COM0A1)|(1 << COM0B1);
+		TCCR0A |= (1 << WGM01)|(1 << WGM00);
+		
+		//Start that SHIT!
+		TCCR0B |= (1 << CS00)|(1 << CS02);
+		
 }
 
-
-void readJoysticks (int* xVal, int* yVal, uint8_t* leftDir, uint8_t* rightDir)
+void readMotorJoysticks (int* xVal, int* yVal, uint8_t* leftDir, uint8_t* rightDir)
 {
 	//variables
-	long ch0Rd, ch1Rd;
+	int ch0Rd, ch1Rd;
+	int readMax = 1024;
+	int readMin = 0;
+	int rdBuffer = 5;
+	
 	
 	ch0Rd = readADC(0);
 
 	ch1Rd = readADC(1);
-
-
 	
 	//map the adc reads and determine direction
-	if (ch0Rd < 32650)
+	
+	//channel 0 lower
+	if (ch0Rd <= ((readMax/2) - rdBuffer))
 	{
-		*xVal = (mapRev(ch0Rd, 32649, 0, 255, 0) - 254);
+		*xVal = abs((map(ch0Rd, readMin, ((readMax/2) - rdBuffer), 0, 254)) - 254);
 		*leftDir = 0;
 	}
-	if (ch0Rd >=32850)
+	
+	//channel 0 higher
+	if (ch0Rd >=((readMax/2) + rdBuffer))
 	{
-		*xVal = map(ch0Rd, 32850, 65600, 0, 255);
+		*xVal = map(ch0Rd, ((readMax/2) + rdBuffer), readMax, 0, 255);
 		*leftDir = 1;
 	}
-	if (ch0Rd > 32650 && ch0Rd < 32850)
+	
+	//channel 0 centered
+	if (ch0Rd >= ((readMax/2) - (rdBuffer + 1)) && ch0Rd <= ((readMax/2) - (rdBuffer - 1)))
 	{
 		*xVal = 0;
 		*leftDir = 0;
 	}
 	
-	if (ch1Rd < 32650)
+	//channel 1 lower
+	if (ch1Rd <= ((readMax/2) - rdBuffer))
 	{
-		*yVal = (mapRev(ch1Rd, 32649, 0, 255, 0) - 254);
+		*yVal = abs((map(ch1Rd, readMin, ((readMax/2) - rdBuffer), 0, 254)) - 254);
 		*rightDir = 0;
 	}
-	if (ch1Rd >= 32850)
+	
+	//channel 1 higher
+	if (ch1Rd >= ((readMax/2) + rdBuffer))
 	{
-		*yVal = map(ch1Rd, 32850, 65600, 0, 255);
+		*yVal = map(ch1Rd, ((readMax/2) + rdBuffer), readMax, 0, 255);
 		*rightDir = 1;
 	}
-	if (ch1Rd >32650 && ch1Rd < 32850)
+	
+	//channel 1 centered
+	if (ch1Rd >= ((readMax/2) - (rdBuffer + 1)) && ch1Rd <= ((readMax/2) - (rdBuffer - 1)))
 	{
 		*yVal = 0;
 		*rightDir = 0;
